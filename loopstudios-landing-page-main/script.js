0236171be7c30ee675ba6e30eba6962c6c006bbf
pavankalyan.c <pavankalyan141640@gmail.com>
2021-05-20 17:54:57 +0530

    keyframes updated

--- loopstudios-landing-page-main/script.js
+++ loopstudios-landing-page-main/script.js
@@ -7,3 +7,22 @@ function closeMenu() {
     document.getElementById("main").classList.remove("hide");
     document.getElementById("hamMenu").classList.add("hide");
 }
+
+document.addEventListener(
+    "scroll",
+    function(e) {
+        var h1 = document.getElementById("second-page");
+
+        // Get it's position in the viewport
+        var bounding = h1.getBoundingClientRect();
+
+        // Log the results
+        console.log(bounding);
+        if (bounding.top < 0) {
+            console.log("-------------------------------");
+            document.getElementById("card1").style.animation =
+                "leftmove 1s ease-out;";
+        }
+    },
+    true
+);
