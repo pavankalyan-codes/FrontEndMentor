63273563fd7e5c5d93d35e5c030089a9545f6903
pavankalyan.c <pavankalyan141640@gmail.com>
2021-05-31 12:16:35 +0530

    clock changes for numbers in dial board. loader changes

--- Clock/script.js
+++ Clock/script.js
@@ -3,6 +3,14 @@ var secondsDeg = 0;
 var minutedDeg = 0;
 var hoursDeg = 0;
 
+document.onreadystatechange = function() {
+    if (document.readyState === "complete") {
+        setTimeout(() => {
+            document.getElementById("loader").style.display = "none";
+        }, 1500);
+    }
+};
+
 const randomColor = () => {
     return "#" + Math.floor(Math.random() * 16777215).toString(16);
 };
@@ -21,13 +29,14 @@ function rotate() {
         "rotate(" + ((90 + seconds * 6) % 360) + "deg)";
     let hourChange =
         ((90 + (hours % 12) * 30) % 360) + ((minutes / 60) * 100 * 30) / 100;
-    console.log(hourChange);
+    let minuteChange =
+        ((90 + minutes * 6) % 360) + ((seconds / 60) * 100 * 6) / 100;
+
     document.getElementById("hours-hands").style.transform =
         "rotate(" + hourChange + "deg)";
     document.getElementById("minutes-hands").style.transform =
-        "rotate(" + ((90 + minutes * 6) % 360) + "deg)";
-    console.log(hours);
-    console.log(minutes);
+        "rotate(" + minuteChange + "deg)";
+
     if (seconds == 0) {
         let currentGradient =
             "linear-gradient(" +
